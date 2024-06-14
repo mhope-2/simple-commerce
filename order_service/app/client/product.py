1364a34331198544688d8dc332baa2c702241f41
mhope <hopemichael720@gmail.com>
2024-06-14 15:05:59 +0000

    Add client service requests

---
+++ order_service/app/client/product.py
@@ -0,0 +1,67 @@
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+import httpx
+from fastapi import HTTPException, status
+from tenacity import retry, stop_after_attempt, stop_after_delay, wait_fixed
+
+
+@dataclass
+class Product:
+    """Class to represent a Product object"""
+
+    code: str
+    name: str
+    price: float
+
+
+class ProductService:
+    """
+    Utility class to define requests to the Product Service
+    """
+
+    @staticmethod
+    @retry(
+        stop=(stop_after_attempt(3) | stop_after_delay(5)),  # stop after 3 attempts or 5 seconds
+        wait=wait_fixed(2)  # wait 2 seconds between retries
+    )
+    async def fetch_product(code: str) -> Optional[Product]:
+        product_service_url = os.getenv("PRODUCT_SERVICE_URL")
+
+        async with httpx.AsyncClient() as client:
+            try:
+                async with client.get(f"{product_service_url}/{code}") as response:
+                    if response.status_code == status.HTTP_200_OK:
+                        data = response.json()
+                        return Product(**data)
+
+                    elif response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR:
+                        raise HTTPException(
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Product service returned a server error",
+                        )
+
+                    elif response.status_code == status.HTTP_404_NOT_FOUND:
+                        raise HTTPException(
+                            status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Product service returned 404",
+                        )
+
+                    else:
+                        raise HTTPException(
+                            status_code=response.status_code,
+                            detail=f"Received non-200 status code from Product Service: {response.status_code}",
+                        )
+
+            except httpx.TimeoutException:
+                raise HTTPException(
+                    status_code=status.HTTP_408_REQUEST_TIMEOUT,
+                    detail="Request to product service timed out"
+                )
+
+            except httpx.RequestException as e:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Request to product service failed: {str(e)}"
+                )
---
+++ order_service/app/client/user.py
@@ -0,0 +1,64 @@
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+import httpx
+from fastapi import HTTPException, status
+from tenacity import retry, stop_after_attempt, stop_after_delay, wait_fixed
+
+
+@dataclass
+class User:
+    """Class to represent a User object"""
+
+    id: str
+    first_name: str
+    last_name: str
+
+
+class UserService:
+    """Utility class to define requests to the User service"""
+
+    @staticmethod
+    @retry(
+        stop=(stop_after_attempt(3) | stop_after_delay(5)),  # stop after 3 attempts or 5 seconds
+        wait=wait_fixed(2)  # wait 2 seconds between retries
+    )
+    async def fetch_user(id: str) -> Optional[User]:
+        user_service_url = os.getenv("USER_SERVICE_URL")
+
+        async with httpx.AsyncClient() as client:
+            try:
+                async with client.get(f"{user_service_url}/{id}") as response:
+                    if response.status_code == status.HTTP_200_OK:
+                        data = response.json()
+                        return User(**data)
+
+                    elif response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR:
+                        raise HTTPException(
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="User service returned a server error",
+                        )
+
+                    elif response.status_code == status.HTTP_404_NOT_FOUND:
+                        raise HTTPException(
+                            status_code=status.HTTP_404_NOT_FOUND,
+                            detail="User service returned a 404"
+                        )
+
+                    else:
+                        raise HTTPException(
+                            status_code=response.status_code,
+                            detail=f"Received a non-200 status code from UserService",
+                        )
+            except httpx.TimeoutException:
+                raise HTTPException(
+                    status_code=status.HTTP_408_REQUEST_TIMEOUT,
+                    detail="Request to User Service timed out",
+                )
+
+            except httpx.RequestException as e:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Request to User Service failed: {str(e)}",
+                )

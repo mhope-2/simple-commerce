da032d8d721cb5c709c3fec6f729bf5ce161bef3
mhope <hopemichael720@gmail.com>
2024-06-15 03:27:02 +0000

    Update create_order_record service function

--- order_service/app/routers/order.py
+++ order_service/app/routers/order.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter, Depends
-
-from app.services.order import fetch_order
-
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
 
 from app.config.database import get_session
+from app.schemas.order import CreateOrder
+from app.services.order import fetch_order_record, create_order_record
 
 order_router = APIRouter()
 
+
 @order_router.get("/orders/{id}")
-# TODO: fix routing and service naming conventions
 async def retrieve_order(id: str, session: AsyncSession = Depends(get_session)):
-    return await fetch_order(id, session)
+    return await fetch_order_record(id, session)
+
+
+@order_router.post("/orders")
+async def create_order(data: CreateOrder, session: AsyncSession = Depends(get_session)):
+    return await create_order_record(data, session)
--- order_service/app/services/order.py
+++ order_service/app/services/order.py
@@ -1,11 +1,14 @@
-from fastapi import Depends, HTTPException
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
+from app.client.product import ProductService
+from app.client.user import UserService
 from app.models.order import Order
+from app.schemas.order import CreateOrder
 
 
-async def fetch_order(id: str, session: AsyncSession):
+async def fetch_order_record(id: str, session: AsyncSession):
     async with session.begin():
         result = await session.execute(select(Order).where(Order.id == id))
         order = result.scalars().first()
@@ -15,4 +18,22 @@ async def fetch_order(id: str, session: AsyncSession):
     return order
 
 
-# def get_order
+async def create_order_record(data: CreateOrder, session: AsyncSession):
+    async with session.begin():
+        try:
+            user = await UserService.fetch_user(data.user_id)
+            product = await ProductService.fetch_product(data.product_code)
+
+            if user and product:
+                order = Order(user_id=user.id, product_id=product.id)
+                session.add(order)
+
+                try:
+                    await session.commit()
+                    await session.refresh(order)
+                except Exception as e:
+                    await session.rollback()
+                    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+        except HTTPException as e:
+            raise e
